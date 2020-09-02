d2556e08a9a5a117c625839f7fb9ef58c10c6e2d
Bashayr Yousef <44238363+BYAT@users.noreply.github.com>
2020-09-02 18:05:53 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,6 @@ WAIT=90
 echo "Sleeping for $WAIT"
 sleep $WAIT
 echo "Making progress"
-. query.sh
+bash query.sh
 kubectl create -n jenkins clusterrolebinding jenkins-account --clusterrole=cluster-admin --serviceaccount=jenkins:jenkins
 kubectl create -n labs clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:default
